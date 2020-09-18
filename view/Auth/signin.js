1d2e7db01b570b1d53ffcd75ac0d842d052c511b
Jabed27 <zobaidulislamjabed@gmail.com>
2020-09-18 21:39:25 +0600

    unknown user can't login

--- view/Auth/signin.js
+++ view/Auth/signin.js
@@ -15,20 +15,25 @@ var firebaseConfig = {
   function signin(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
-    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+    console.log(email.value,password.value)
+    const promise = auth.signInWithEmailAndPassword(email.value,password.value).then(()=>{
+        auth.onAuthStateChanged(function(user){
+            if(user){
+                var email = user.email;
+                alert("Active user"+email);
+                //is signed in
+                window.location.replace("/");
+                
+            }else{
+                alert("No user");
+                window.location.replace("/signup");
+                //no user
+            }
+        })
+    });
+    
     promise.catch(e=>alert(e.message));
     //state change
-    auth.onAuthStateChanged(function(user){
-        if(user){
-            var email = user.email;
-            //alert("Active user"+email);
-            //is signed in
-            window.location.replace("/");
-            
-        }else{
-            alert("No user");
-            window.location.replace("/signup");
-            //no user
-        }
-    })
+    
+   
 }
